774719f8cef529e4cfe0517255b6877db55bfb2e
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2026-08-02 00:32:55 +0200

    add openrgb-service option

--- default.nix
+++ default.nix
@@ -1,4 +1,25 @@
 {
+  config,
+  lib,
   pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") { },
+  ...
 }:
-pkgs.callPackage ./package.nix { }
+let 
+  openrgb-fade = pkgs.callPackage ./package.nix { };
+  cfg = config.services.openrgb-fade;
+in
+{
+  options.services.openrgb-fade.enable = lib.mkEnableOption "openrgb-fade user service";
+
+  config = lib.mkIf config.services.openrgb-fade.enable {
+    systemd.user.services.openrgb-fade = {
+      enable = true;
+      description = "OpenRGB-Fade user service.";
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${openrgb-fade}/bin/openrgb-fade";
+      };
+      wantedBy = [ "default.target" ];
+    };
+  };
+} 
